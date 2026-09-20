e2957dcec09a1b8bc0274377db5a206e89e3b745
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-09-20 07:51:23 +0200

    feat(site): pin hub publisher to plinth trunk with project_grid
    Moves the site input from the feat/machine-index branch rev to trunk
    f9d40ee, which now carries project_grid, the machine-index artifacts,
    and the host-pinning guard. Relock is codeberg-free.

--- site/flake.nix
+++ site/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     plinth = {
-      url = "git+https://github.com/caniko/plinth.git?ref=refs/heads/feat/machine-index&rev=d848aa7566f913e3c46b37b9f02654745b2bf482";
+      url = "git+https://github.com/caniko/plinth.git?ref=refs/heads/trunk&rev=f9d40ee3263a4fb371eb85e06818c206a7905446";
     };
   };
 
